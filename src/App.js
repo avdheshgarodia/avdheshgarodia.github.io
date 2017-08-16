cbb7ab6ea869182659a45b61d57c74ebe5e06372
Amanpreet Singh <apsdehal@gmail.com>
2017-08-16 13:11:35 -0400

    Add basic framework for displaying images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import Masonry from 'react-masonry-component';
+
 import logo from './logo.svg';
 import './App.css';
+import images from "./images.json";
 
 class App extends Component {
   render() {
+    let categories = images.images.categories;
+    let headerElements = Object.keys(categories).map((category, index) => {
+      return (
+        <li key={index} className="image-element-class">
+          <img src={categories[category].header}/>
+          <div className="contents">
+            <a href="">View {category}</a>
+          </div>
+        </li>
+      )
+    })
+
+    let masonryOptions = {
+        transitionDuration: 0
+    };
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Avdhesh Garodia</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <Masonry
+              elementType={'ul'}
+              options={masonryOptions}>
+              {headerElements}
+          </Masonry>
+        </div>
       </div>
     );
   }
